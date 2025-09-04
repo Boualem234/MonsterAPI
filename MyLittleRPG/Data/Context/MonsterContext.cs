95f78548ed0f0fc627b3c53ba714072e30f03fa6
Boualem234 <2534056@etu.cchic.ca>
2025-09-04 10:48:47 -0400

    initial

---
+++ MyLittleRPG/Data/Context/MonsterContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLittleRPG_ElGuendouz.Models;
+
+namespace MyLittleRPG_ElGuendouz.Data.Context
+{
+    public class MonsterContext : DbContext
+    {
+        public DbSet<Monster> Monsters { get; set; } // factorie
+
+        public MonsterContext(DbContextOptions<MonsterContext> options) : base(options) { }
+    }
+}
